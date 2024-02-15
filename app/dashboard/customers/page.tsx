6d57ab87d2e729c3bf551d39f34121ba0d6c1480
hycgh <960213661@qq.com>
2024-02-15 13:09:52 +0800

    新增：客户列表

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers | Acme Dashboard',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const customers = await fetchFilteredCustomers(query);
+  return <CustomersTable customers={customers} />;
 }
